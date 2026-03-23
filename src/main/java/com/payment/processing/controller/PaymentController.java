d6769e920c0eebfc0bc765119fcd93a91c2c3521
roshniLingam <lingam_roshni.ghrcecs@raisoni.net>
2026-03-23 22:52:24 +0530

    Added Controller

---
+++ src/main/java/com/payment/processing/controller/PaymentController.java
@@ -0,0 +1,47 @@
+package com.payment.processing.controller;
+
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.payment.processing.payload.CreatePaymentRequest;
+import com.payment.processing.payload.PaymentResponse;
+import com.payment.processing.service.PaymentService;
+
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import lombok.NonNull;
+
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/payments")
+public class PaymentController {
+    @Autowired
+    private PaymentService paymentService;
+
+    @PostMapping
+    public ResponseEntity<PaymentResponse> createPayment(
+            @Valid @RequestBody CreatePaymentRequest request,
+            @RequestHeader("Idempotency-Key") String idempotencyKey) {
+        PaymentResponse response =
+                paymentService.createPayment(request, idempotencyKey);
+
+        return ResponseEntity.ok(response);
+    }
+    
+    @GetMapping("/{paymentId}/status")
+    public ResponseEntity<PaymentResponse> getPaymentStatus(@NonNull @PathVariable UUID paymentId) {
+
+        PaymentResponse response = paymentService.getPaymentStatus(paymentId);
+        return ResponseEntity.ok(response);
+    }
+}
