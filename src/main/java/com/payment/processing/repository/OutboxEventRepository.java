d2b153c940172631bad61cf49bb22ee456e91471
roshniLingam <lingam_roshni.ghrcecs@raisoni.net>
2026-05-07 01:18:17 +0530

    DB Locking and ad new query

--- src/main/java/com/payment/processing/repository/OutboxEventRepository.java
+++ src/main/java/com/payment/processing/repository/OutboxEventRepository.java
@@ -4,12 +4,16 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.stereotype.Repository;
 
 import com.payment.processing.entity.OutboxEvent;
 import com.payment.processing.enums.OutboxStatus;
 
+import jakarta.persistence.LockModeType;
+
 @Repository
 public interface OutboxEventRepository extends JpaRepository<OutboxEvent, UUID>{
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     List<OutboxEvent> findTop100ByStatus(OutboxStatus status);
 }
--- src/main/java/com/payment/processing/repository/PaymentRepository.java
+++ src/main/java/com/payment/processing/repository/PaymentRepository.java
@@ -1,15 +1,30 @@
 package com.payment.processing.repository;
 
+import java.time.Instant;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.payment.processing.entity.Payment;
+
+import jakarta.persistence.LockModeType;
 
 @Repository
 public interface PaymentRepository extends JpaRepository<Payment, UUID> {
     Optional<Payment> findByIdempotencyKey(String idempotencyKey);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("""
+        SELECT p FROM Payment p
+        WHERE p.status = 'PENDING'
+        AND (p.nextRetryAt IS NULL OR p.nextRetryAt <= :now)
+    """)
+    List<Payment> findRetryablePayments(@Param("now") Instant now);
 
 }
